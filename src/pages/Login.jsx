c62f6f330fca23dc1e128c97bc5ffe329b1e8373
DanyaSantos <santosresendezde1720@gmail.com>
2023-06-15 20:29:28 -0600

    commit inicial

---
+++ src/pages/Login.jsx
@@ -0,0 +1,62 @@
+
+import { useState,useEffect } from "react"
+import { FaSignInAlt} from "react-icons/fa"
+
+const Login = () => {
+  const [formData, setFormData]= useState({
+    email: '',
+    password: ''
+  })
+  const {email, password} = formData
+  const onChange = (e) =>{ 
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+  const onSubmit = (e) =>{
+    e.preventDefault()
+   }
+  return (
+    <>
+      <section className="heading">
+        <h4>
+            <FaSignInAlt /> Login
+        </h4>
+        <p>Por favor teclea tu email y contraseña</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+         
+          <div className="form-group">
+          <input 
+          type="email" 
+          className="form-control" 
+          id="email" 
+          name="email" 
+          value={email} 
+          onChange={onChange}
+          placeholder="por favor teclea tu email"/>
+          </div>
+
+          <div className="form-group">
+          <input 
+          type="password" 
+          className="form-control" 
+          id="password" 
+          name="password" 
+          value={password} 
+          onChange={onChange}
+          placeholder="por favor teclea tu password"/>
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
+}
+
+export default Login
